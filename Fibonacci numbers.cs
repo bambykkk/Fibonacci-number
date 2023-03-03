e11f203aff68e600b111bef606f6031a99a7d3db
bambykkk <115668904+bambykkk@users.noreply.github.com>
2023-03-04 02:15:32 +0400

    Create Fibonacci numbers.cs

---
+++ Fibonacci numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of Fibonacci numbers");
+            int numbers1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            ulong x = 1;
+            ulong y = 1;
+            ulong sum = 0;
+
+            for (int i = 0; i < numbers1; i++)
+            {
+                x = sum;
+                sum = x + y;
+                y=x;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
